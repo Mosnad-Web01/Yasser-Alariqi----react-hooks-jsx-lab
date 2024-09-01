31aa12280ff6a6dc3b403835d764cc8537d2e1fe
YasserEsam <yaaser10esam1010@gmail.com>
2024-09-01 14:24:38 +0300

    done and pass the test by writing the required jsx code

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Yasser Esam, the greatest developer in the room. " I am the only one in that room "</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
